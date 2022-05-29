da4b9441058bb8850631679b2158d58d9d0b60c7
rokib97 <hasanrokib1997@gmail.com>
2022-05-29 19:14:48 +0600

    basic types number string boolean and intrface

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from "react";
 import "./App.css";
 
+// declare type with variable
 let student: string = "Rokib";
 let age: number = 20;
 let isSmart: boolean = true;
+console.log(student, age, isSmart);
 
+// declare array of string
 let students: string[] = ["Rahim", "Karim", "Jashim"];
-let fees: number[] = [12, 17, 20, 21];
+console.log(students);
 
+// declare array of numbers
+let fees: number[] = [12, 17, 20, 21];
+console.log(fees);
+
+// declare interface how object will look like
 interface Person {
   name: string;
   Job: string;
   age?: number; // optional property
 }
 
+// using interface with object type
 const person: Person = {
   name: "Rokib",
   Job: "Development",
 };
 console.log(person);
+
+// object type declare
+const person2: { name: string; job: string; age: number } = {
+  name: "ali",
+  job: "Programmer",
+  age: 21,
+};
+console.log(person2);
+
 function App() {
   return <div className="App"></div>;
 }
