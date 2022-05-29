f5cb369009a14ceb3fbc7ac98f0eecc12ba8c4b8
rokib97 <hasanrokib1997@gmail.com>
2022-05-29 19:01:24 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+let student: string = "Rokib";
+let age: number = 20;
+let isSmart: boolean = true;
+
+let students: string[] = ["Rahim", "Karim", "Jashim"];
+let fees: number[] = [12, 17, 20, 21];
+
+interface Person {
+  name: string;
+  Job: string;
+  age?: number; // optional property
+}
+
+const person: Person = {
+  name: "Rokib",
+  Job: "Development",
+};
+console.log(person);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/Vanila/Vanila.js
@@ -0,0 +1,8 @@
+let user = "Robert Junior";
+user = 55;
+user = false;
+user = { id: 1, age: 25, address: "Dhaka" };
+user = [1, 2, 4, 5];
+user = ["john", "Akash", "Sindhu"];
+user = () => 55;
+console.log(user);
